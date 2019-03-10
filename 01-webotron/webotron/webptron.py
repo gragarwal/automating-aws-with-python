1d07794fd960303d87e31d01c3d9337e12c51d93
unknown <gragarwal@gmail.com>
2019-03-10 19:38:08 -0400

    adding kitten web

--- 01-webotron/webotron/webptron.py
+++ 01-webotron/webotron/webptron.py
@@ -1,5 +1,9 @@
 import boto3
 import click
+from botocore.exceptions import ClientError
+from pathlib import Path
+import mimetypes
+
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
 
@@ -15,10 +19,68 @@ def list_buckets():
         print(bucket)
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
+
 def list_bucket_objects(bucket):
         "List objects in an s3 bucket"
         for obj in s3.Bucket(bucket).objects.all():
             print(obj)
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    "Create and configure s3 bucket"
+    s3_bucket = s3.create_bucket(
+        Bucket=bucket
+    )
+    policy = """
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "PublicReadGetObject",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": "s3:GetObject",
+                "Resource": "arn:aws:s3:::%s/*"
+            }
+        ]
+    }
+    """% s3_bucket.name
+    policy = policy.strip()
+
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+    ws = s3_bucket.Website()
+    ws.put(WebsiteConfiguration={
+    'ErrorDocument': {
+            'Key': 'error.html'
+        },
+        'IndexDocument': {
+            'Suffix': 'index.html'
+      }
+      }
+      )
+def upload_file(s3_bucket, path, key):
+    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+    s3_bucket.upload_file(
+    path,
+    key,
+    ExtraArgs={
+    'ContentType': 'test/html'
+    })
+@cli.command('sync')
+@click.argument('pathname', type=click.Path(exists=True))
+@click.argument('bucket')
+def sync(pathname,bucket):
+    "Sync comtents of PATHNAME to BUCKET"
+    root = Path(pathname).expanduser().resolve()
+    s3_bucket = s3.Bucket(bucket)
+
+    def handle_directory(target):
+        for p in target.iterdir():
+           if p.is_dir(): handle_directory(p)
+           if p.is_file(): upload_file(s3_bucket, str(p),str(p.relative_to(root)))
+
+    handle_directory(root)
 
 
 if __name__ == '__main__':
